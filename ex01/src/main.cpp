882d9ec162eeb287062c9b727ad85d4005d400b5
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-23 18:38:02 -0500

    testing

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -5,10 +5,23 @@
 int main(void)
 {
 	Span sp = Span(5);
+
+	try
+	{
+		std::cout << CYAN << "TEST 1" << DEF << std::endl;
+		sp.addNumber(6);
+		std::cout << YELLOW << "Calculating shortest span" << DEF << std::endl;
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << RED << e.what() << DEF << std::endl;
+	}
+	std::cout << std::endl;
 	
 	try
 	{
-		sp.addNumber(6);
+		std::cout << CYAN << "TEST 2" << DEF << std::endl;
 		sp.addNumber(3);
 		sp.addNumber(17);
 		sp.addNumber(9);
@@ -20,9 +33,23 @@ int main(void)
 	{
 		std::cout << RED << e.what() << DEF << std::endl;
 	}
+
+	std::cout << CYAN << "Contents are:" << DEF << std::endl;
 	sp.print(sp.getVector());
-	std::cout << YELLOW << "Calculating shortest span" << DEF << std::endl;
-	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << std::endl;
+
+	try
+	{
+		std::cout << CYAN << "TEST 3" << DEF << std::endl;
+		std::cout << YELLOW << "Calculating shortest span" << DEF << std::endl;
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << RED << e.what() << DEF << std::endl;
+	}
+	std::cout << std::endl;
+
 //	std::cout << sp.longestSpan() << std::endl;
 	return (0);
 }
