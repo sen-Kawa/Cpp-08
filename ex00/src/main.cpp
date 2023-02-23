b70b4b9ffbd2f145f0cde43d7f0690d11c1db059
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-22 20:11:58 -0500

    makefile

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <iostream>
 #include <algorithm>
 #include "../header/colours.hpp"
 
@@ -6,6 +7,8 @@ template<typename T>
 void easyfind(T ints, int tofind)
 {
 	auto found {std::find(ints.begin(), ints.end(), tofind)};	
+	if (found == ints.end())
+		std::cout << RED << "Couldn't find " << tofind << DEF << std::endl;
 
 	return ;
 }
@@ -13,6 +16,8 @@ void easyfind(T ints, int tofind)
 int main(void)
 {
 	std::array<int, 6> ints = {1, 2, 3, 4, 5, 6};
+	for (int i : ints)
+		std::cout << i << " ";
 	return (0);
 }
 
