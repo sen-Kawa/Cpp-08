d80eac0f6ee5e4a69ea4b41e6e88b93cc07653d6
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-22 20:03:47 -0500

    colours

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,10 +1,18 @@
 #include "../header/.class.hpp"
 #include <array>
+#include <algorithm>
 #include "../header/colours.hpp"
+
+template<typename T>
+void easyfind(T ints, int tofind)
+{
+	auto found {std::find(ints.begin(), ints.end(), tofind)};	
+	return ;
+}
 
 int main(void)
 {
-	std::array<int, 6> ar = {1, 2, 3, 4, 5, 6};
+	std::array<int, 6> ints = {1, 2, 3, 4, 5, 6};
 	return (0);
 }
 
