5b167704ffd113721dba43a2a0be69da786a1346
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-22 21:15:57 -0500

    methods in span class

--- ex01/header/Span.class.hpp
+++ ex01/header/Span.class.hpp
@@ -2,18 +2,24 @@
 # define SPAN_CLASS_HPP
 
 #include <iostream>
+#include <vector>
 #include "colours.hpp"
 
 class Span
 {
 	private:
 
+		const unsigned int	maxInts;
+		std::vector<int>	ints;
 
 	public:
 
-		int	getSpan(void) const;
+		void	addNumber(int n);
+		void	shortestSpan(void) const;
+		void	longestSpan(void) const;
 
 		Span(void);
+		Span(const unsigned int);
 		Span(Span const &src);
 		Span&	operator=(Span const &assign);
 		~Span(void);
