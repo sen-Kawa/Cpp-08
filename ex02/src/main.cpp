eb9e262d4fb2edfe228376107f70a432b1cdd995
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-24 13:56:40 -0500

    testing

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -4,6 +4,7 @@
 int main(void)
 {
 
+	std::cout << CYAN << "Creating mstack object..." << DEF << std::endl;
 	MutantStack<int>	mstack;
 	std::cout << YELLOW << "Pushing 5..." << DEF << std::endl;
 	mstack.push(5);
@@ -13,7 +14,7 @@ int main(void)
 	std::cout << mstack.top() << std::endl;
 	std::cout << YELLOW << "Popping..." << DEF << std::endl;
 	mstack.pop();
-	std::cout << CYAN << "Calculating size..." << DEF << std::endl;
+	std::cout << CYAN << "Calculating size of mstack..." << DEF << std::endl;
 	std::cout << mstack.size() << std::endl;
 	std::cout << YELLOW << "Pushing 3..." << DEF << std::endl;
 	mstack.push(3);
@@ -24,23 +25,29 @@ int main(void)
 	//[...]
 	std::cout << YELLOW << "Pushing 0..." << DEF << std::endl;
 	mstack.push(0);
+	std::cout << CYAN << "Calculating size of mstack..." << DEF << std::endl;
+	std::cout << mstack.size() << std::endl;
+	std::cout << YELLOW << "Declaring iterators..." << DEF << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	std::cout << CYAN << "Before modify" << DEF << std::endl;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
 	++it;
 	--it;
-	std::cout << CYAN << "After modify" << DEF << std::endl;
+	std::cout << CYAN << "Printing with iterator" << DEF << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
+	std::cout << CYAN << "Instantiating with copy constructor..." << DEF << std::endl;
 	std::stack<int> s(mstack);
+	std::cout << CYAN << "Calculating size of s..." << DEF << std::endl;
+	std::cout << s.size() << std::endl;
+	std::cout << YELLOW << "Popping from mstack..." << DEF << std::endl;
+	mstack.pop();
+	std::cout << CYAN << "Calculating size of s..." << DEF << std::endl;
+	std::cout << s.size() << std::endl;
+	std::cout << CYAN << "Calculating size of mstack..." << DEF << std::endl;
+	std::cout << mstack.size() << std::endl;
 	return (0);
 }
 
