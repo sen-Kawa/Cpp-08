9bdafd6b386f899f9ffdb41b613e4307b7578dba
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-24 12:52:21 -0500

    iterator defs

--- ex02/header/MutantStack.class.hpp
+++ ex02/header/MutantStack.class.hpp
@@ -8,17 +8,29 @@
 template<typename T>
 class MutantStack : public std::stack<T>
 {
-	private:
-
-
 	public:
-
-		int	getMutantStack(void) const;
 
 		MutantStack(void);
 		MutantStack(MutantStack const &src);
 		MutantStack&	operator=(MutantStack const &assign);
-		~MutantStack(void);
+		virtual ~MutantStack(void);
+
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+
+		iterator begin(void)
+		{
+			return (this->c.begin());
+		}
+		iterator				end(void);
+		const_iterator			begin(void) const;
+		const_iterator			end(void) const;
+		reverse_iterator		rbegin(void);
+		reverse_iterator		rend(void);
+		const_reverse_iterator	rbegin(void) const;
+		const_reverse_iterator	rend(void) const;
 };
 
 #endif
